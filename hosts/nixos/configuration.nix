66db00df2bdbbbe509d804fe644b1f94fc981f36
Thecreeper55 <colormorado75@gmail.com>
2025-12-06 17:07:10 +0100

    added wireguard

--- hosts/nixos/configuration.nix
+++ hosts/nixos/configuration.nix
@@ -14,6 +14,7 @@
     ../../nixos/tailscale.nix
     ../../nixos/hyprland.nix
     ../../nixos/wireshark.nix
+    ../../nixos/wireguard.nix
 
     # You should let those lines as is
     ./hardware-configuration.nix
