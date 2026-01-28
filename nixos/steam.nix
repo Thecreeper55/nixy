f684a1185580a901fea65251dab1bb44da28d23b
Thecreeper55 <Thecreeper55@mail.com>
2026-01-28 13:51:20 +0100

    steam vr config

--- nixos/steam.nix
+++ nixos/steam.nix
@@ -1,10 +1,36 @@
 {pkgs, ...}: {
-  programs.steam = {
-    enable = true;
-    extraCompatPackages = with pkgs; [
-      proton-ge-bin
-    ];
+  programs = {
+    gamescope = {
+      enable = true;
+      capSysNice = true;
+    };
+
+    steam = {
+      enable = true;
+      remotePlay.openFirewall = true;
+      dedicatedServer.openFirewall = true;
+      gamescopeSession.enable = true;
+      enable32Bit = true;
+
+      extraCompatPackages = with pkgs; [
+        proton-ge-bin
+      ];
+    };
+
+    git = {
+      lfs.enable = true;
+    };
   };
 
   hardware.opengl.enable = true;
+
+  services.monado = {
+    enable = true;
+    defaultRuntime = true;
+  };
+
+  systemd.user.services.monado.environment = {
+    STEAM_LH_ENABLE = "1";
+    XRT_COMPOSITOR_COMPUTE = "1";
+  };
 }
